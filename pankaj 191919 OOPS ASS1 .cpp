812f1fdbff8cf95d1697b5ca715bf89ce3f38256
Pankaj100ni <59951275+Pankaj100ni@users.noreply.github.com>
2021-02-25 22:16:05 +0530

    Add files via upload

---
+++ pankaj 191919 OOPS ASS1 .cpp
@@ -0,0 +1,44 @@
+//PROGRAM OOPS ASS...
+//1. Write the program to read a matrix from your keyboard and display the same by using function.
+// BY- PANKAJ (191919) Btech CSE (IV) semester.
+#include<iostream>
+using namespace std;
+int read_matrix(int a[10][10],int x,int y);
+int display_matrix( int a[10][10] ,int x, int y);
+int main()
+{
+int m,n;
+cout <<"Enter the value of M and N:";
+cin >>m >> n;
+cout << endl;
+int a[10][10];
+read_matrix(a,m,n);
+display_matrix(a,m,n);
+}
+int read_matrix( int a[10][10],int x,int y)
+{
+cout<<" enter the values of the matrix" <<"\t";
+int i,j;
+for(i=0;i<x;i++)
+{
+for(j=0;j<y;j++)
+{
+cin>>a[i][j];
+}
+}
+}
+int display_matrix( int a[10][10] ,int x, int y)
+{
+int i,j;
+for(i=0;i<x;i++)
+{
+for(j=0;j<y;j++)
+{
+cout<< a[i][j]<<"\t";
+}
+cout<< endl;
+}
+return 0;
+}
+
+
